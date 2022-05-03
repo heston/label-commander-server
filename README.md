b453d82f18e4c50eb2577dcece19b867a7423bd8
Heston Liebowitz <heston@junelife.com>
2022-05-03 17:31:24 -0600

    Update to Node 16

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # Label Commander Server
 
 Firebase Cloud Function server for [Label Commander client](https://github.com/heston/label-commander).
+
+## Installation
+
+### Configure Cloud Functions
+
+1. On the command line, clone this repo and change into the cloned directory.
+1. Follow the [Firebase Cloud Functions Get Started Guide](https://firebase.google.com/docs/functions/get-started).
+    1. If prompted to overwrite any files during the init process, always select `no`.
+    1. When prompted to select a language, select `Javasceript`.
+1. Set your authentication key by running: `firebase functions:secrets:set IFTTT_SECRETKEY`.
+    1. Follow any prompts to set up Google Cloud Secret Manager.
+    1. Select a long random string to use as an authentication key. It doesn't matter what this is, only that it's a strong     
+       password. Here's a good starting place: `openssl rand -base64 18`.
+    1. Take note of this key, as you will need it later.
+1. Deploy the functions: `npm run deploy`.
+1. Take note of the `Function URL` lines printed near the end. You will need these later.
+
+### Configure IFTTT
+
+To print a label, you need to set up IFTTT to trigger a Webhook action in response to a trigger of your choice.
+
+1. Create a new applet in IFTTT.
+1. Set up the trigger of your choice.
+1. For the action, select "Make a web request."
+1. Set the URL to the `Function URL` parameter printed during deployment. It will be something like, `https://us-central1-myproject.cloudfunctions.net/printBatch`, where `myproject` will be the name of your Firebase project.
+1. Set the method to `POST`.
+1. Set the content type to `application/json`.
+1. Set the body to:
+    ```json
+    { "authentication": "AUTH_KEY", "items": [ {"body": "Text to print", "qty": 1} ] }
+    ```
+    Where `AUTH_KEY` is the authentication key you set earlier. The `body` and `qty` may be ingredients from your trigger.
+1. Click "Update action" to save your applet.
+1. Try it out!
--- functions/index.js
+++ functions/index.js
@@ -1,92 +1,143 @@
-const admin = require('firebase-admin');
-const crypto = require('crypto');
-const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+const crypto = require("crypto");
+const functions = require("firebase-functions");
 
 admin.initializeApp(functions.config().firebase);
 
+/**
+ * Whether the current request is authenticated.
+ *
+ * @param   {string}  key  The authorization token from the request.
+ *
+ * @return  {boolean}       Whether the key is valid.
+ */
 function isAuthorized(key) {
-    const secretkey = functions.config().ifttt.secretkey;
+  const secretkey = process.env.IFTTT_SECRETKEY;
 
-    // Fail closed if key is missing
-    if (!secretkey) {
-        return false;
-    }
+  // Fail closed if key is missing
+  if (!secretkey) {
+    return false;
+  }
 
-    return key === secretkey;
+  try {
+    return crypto.timingSafeEqual(
+        Buffer.from(key, "utf8"),
+        Buffer.from(secretkey, "utf8")
+    );
+  } catch (e) {
+    return false;
+  }
 }
 
+/**
+ * Store a label request in the database.
+ *
+ * @param   {string}  body  The body text of the label.
+ * @param   {number}  qty   The number of labels to print.
+ *
+ * @return  {Promise}        Promise for the async database write.
+ */
 function writeLabel(body, qty) {
-    const hash = crypto.createHash('sha1');
-    hash.update(body);
-    // Get current date and convert to Unix timestamp
-    hash.update(String(Number(new Date())));
-    const job_id = hash.digest('hex')
+  const hash = crypto.createHash("sha1");
+  hash.update(body);
+  // Get current date and convert to Unix timestamp
+  hash.update(String(Number(new Date())));
+  const jobId = hash.digest("hex");
 
-    const key = `print_jobs/${job_id}`;
-    const payload = {
-        text: body,
-        qty: qty,
-    };
-    return admin.database().ref(key).set(payload);
+  const key = `print_jobs/${jobId}`;
+  const payload = {
+    text: body,
+    qty: qty,
+  };
+  return admin.database().ref(key).set(payload);
 }
 
+/**
+ * Function decorator to authenticate an HTTP request.
+ *
+ * @param   {Function}  fcn  HTTP handler function to decorate.
+ *
+ * @return {Function}   The decorated function.
+ */
 function withAuth(fcn) {
-    return function withAuthImpl(req, res) {
-        // Get the pin from the request
-        const secretKey = req.body.authentication;
+  return function withAuthImpl(req, res) {
+    // Get the pin from the request
+    const secretKey = req.body.authentication;
 
-        console.log('print', req.body);
+    if (!isAuthorized(secretKey)) {
+      res.status(401).send("Unauthorized");
+      return;
+    }
 
-        if (!isAuthorized(secretKey)) {
-            res.status(401).send('Unauthorized');
-            return;
-        }
-        
-        fcn(req, res);
-    };
+    fcn(req, res);
+  };
 }
 
+/**
+ * Get label printing parameters from an HTTP Request.
+ *
+ * @param   {Request.body}  body  Body of an HTTP request.
+ *
+ * @return  {Object}        Object with keys: qty, body.
+ */
 function getParams(body) {
-    return {
-        qty: parseInt(body.qty, 10) || 1,
-        body: body.body,
-    };
+  return {
+    qty: parseInt(body.qty, 10) || 1,
+    body: body.body,
+  };
 }
 
+/**
+ * HTTP handler function to print a single label.
+ *
+ * @param   {Request}  req  Express Request object.
+ * @param   {Respobse}  res  Express Response object.
+ */
 const printLabelAction = withAuth((req, res) => {
-    if (!req.body.body) {
-        res.status(400).send('Bad Request');
-        return;
-    }
-    const params = getParams(req.body);
-    
-    writeLabel(params.body, params.qty)
-        .then(() => res.status(200).send('OK'))
-        .catch(() => res.status(503).send('Could not save label to database'));
+  if (!req.body.body) {
+    res.status(400).send("Bad Request");
+    return;
+  }
+  const params = getParams(req.body);
+
+  writeLabel(params.body, params.qty)
+      .then(() => res.status(200).send("OK"))
+      .catch(() => res.status(503).send("Could not save label to database"));
 });
 
+/**
+ * HTTP handler function to print a batch of labels.
+ *
+ * @param   {Request}  req  Express Request object.
+ * @param   {Response}  res  Express Response object.
+ */
 const printBatchAction = withAuth((req, res) => {
-    const items = req.body.items;
-    
-    if (!Array.isArray(items)) {
-        res.status(400).send('Bad Request');
+  const items = req.body.items;
+
+  if (!Array.isArray(items)) {
+    res.status(400).send("Bad Request");
+  }
+
+  const results = [];
+  items.forEach((item) => {
+    if (!item.body) {
+      results.push(Promise.reject(new Error("Invalid body")));
+      return;
     }
-    
-    const results = [];
-    items.forEach((item) => {
-        if (!item.body) {
-            results.push(Promise.reject(new Error('Invalid body')));
-            return;
-        }
-        
-        const params = getParams(item);
-        results.push(writeLabel(params.body, params.qty)); 
-    });
-    
-    Promise.all(results)
-        .then(() => res.status(200).send('OK'))
-        .catch(() => res.status(503).send('Could not save label(s) to database'));
+
+    const params = getParams(item);
+    results.push(writeLabel(params.body, params.qty));
+  });
+
+  Promise.all(results)
+      .then(() => res.status(200).send("OK"))
+      .catch(() => res.status(503).send("Could not save label(s) to database"));
 });
 
-exports.printLabel = functions.https.onRequest(printLabelAction);
-exports.printBatch = functions.https.onRequest(printBatchAction);
+exports.printLabel = functions
+    .runWith({secrets: ["IFTTT_SECRETKEY"]})
+    .https.onRequest(printLabelAction);
+
+exports.printBatch = functions
+    .runWith({secrets: ["IFTTT_SECRETKEY"]})
+    .https.onRequest(printBatchAction);
